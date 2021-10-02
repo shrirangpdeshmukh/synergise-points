1b18317a177c3295d69566c051c51b3d0b1d8015
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2021-10-02 23:42:16 +0530

    Added Issue Checking

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const Table = () => {
   // const [pointsArray, setPointsArray] = useState([]);
   const [PRInfo, setPRInfo] = useState([]);
+  const [issueInfo, setIssueInfo] = useState([]);
   /**
    
    * GitHub ID : {
@@ -32,15 +33,27 @@ const Table = () => {
       .catch((error) => console.error(error));
   };
 
+  const getContributorIssues = () => {
+    axios
+      .get(
+        'https://api.github.com/search/issues?q=org:Dummy-Organ+is:issue+label:"syn-accepted"'
+      )
+      .then((response) => {
+        console.log(response.data);
+        setIssueInfo(response.data.items);
+      })
+      .catch((error) => console.error(error));
+  };
+
   const refactorData = () => {
     console.log("Hii");
 
     const userMap = new Map();
 
-    for (var i = 0; i < PRInfo.length; i++) {
-      if (PRInfo[i].labels.length > 0) {
+    for (var j = 0; j < PRInfo.length; j++) {
+      if (PRInfo[j].labels.length > 0) {
         let increment = 0;
-        for (let label of PRInfo[i].labels) {
+        for (let label of PRInfo[j].labels) {
           if (scoreMap.has(label.name)) {
             increment = scoreMap.get(label.name);
             break;
@@ -50,23 +63,44 @@ const Table = () => {
         console.log(increment);
 
         if (increment > 0) {
-          if (userMap.has(PRInfo[i].user.login)) {
-            const oldData = userMap.get(PRInfo[i].user.login);
+          if (userMap.has(PRInfo[j].user.login)) {
+            const oldData = userMap.get(PRInfo[j].user.login);
             const newData = { ...oldData };
             newData.score = newData.score + increment;
             newData.pr++;
-            userMap.set(PRInfo[i].user.login, newData);
+            userMap.set(PRInfo[j].user.login, newData);
           } else {
             const data = {
-              image: PRInfo[i].user.avatar_url,
+              image: PRInfo[j].user.avatar_url,
               pr: 1,
               issue: 0,
               score: increment,
             };
-            userMap.set(PRInfo[i].user.login, data);
+            userMap.set(PRInfo[j].user.login, data);
           }
         }
       }
+      // console.log(userMap);
+      // console.log(PRInfo);
+    }
+
+    for (var j = 0; j < issueInfo.length; j++) {
+      if (userMap.has(issueInfo[j].user.login)) {
+        const oldData = userMap.get(issueInfo[j].user.login);
+        const newData = { ...oldData };
+        newData.score = newData.score + 4;
+        newData.issue++;
+        userMap.set(issueInfo[j].user.login, newData);
+      } else {
+        const data = {
+          image: issueInfo[j].user.avatar_url,
+          pr: 0,
+          issue: 1,
+          score: 4,
+        };
+        userMap.set(issueInfo[j].user.login, data);
+      }
+
       // console.log(userMap);
       // console.log(PRInfo);
     }
@@ -76,13 +110,14 @@ const Table = () => {
 
   useEffect(() => {
     getPRs();
+    getContributorIssues();
   }, []);
 
   useEffect(() => {
     if (PRInfo.length > 0) {
       refactorData();
     }
-  }, [PRInfo]);
+  }, [PRInfo, issueInfo]);
 
   return null;
 };
