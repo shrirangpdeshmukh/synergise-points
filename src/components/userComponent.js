fe9fa3509cbd6b5d9a78c7bebd4b6430dded6a39
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2021-10-03 01:34:52 +0530

    user details

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -2,13 +2,14 @@ import {
   Avatar,
   makeStyles,
   Card,
-  CardMedia,
+  CardHeader,
   CardActions,
   Typography,
   CardContent,
   Button,
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
   row: {
@@ -34,24 +35,123 @@ const useStyles = makeStyles(() => ({
 
 const UserComponent = () => {
   const classes = useStyles();
-  const [author, setAuthor] = useState(null);
+  const [userImage, setUserImage] = useState(null);
+  const [points, setPoints] = useState(0);
+  const [PR, setPR] = useState(0);
+  const [issues, setIssues] = useState(0);
+  const [PRInfo, setPRInfo] = useState(null);
+  const [issueInfo, setIssueInfo] = useState(null);
+  const [PRArray, setPRArray] = useState([]);
+  const [issueArray, setIssueArray] = useState([]);
+
+  const scoreMap = new Map([
+    ["syn-easy", 10],
+    ["syn-medium", 25],
+    ["syn-hard", 40],
+  ]);
+
+  const getPRs = (userId) => {
+    axios
+      .get(
+        `https://api.github.com/search/issues?q=org:Dummy-Organ+is:pr+is:merged+label:"syn-accepted"+author:"${userId}"`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setPR(response.data.total_count);
+        setPRInfo(response.data.items);
+        if (userImage === null && response.data.total_count > 0) {
+          setUserImage(response.data.items[0].user.avatar_url);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const getContributorIssues = (userId) => {
+    axios
+      .get(
+        `https://api.github.com/search/issues?q=org:Dummy-Organ+is:issue+label:"syn-accepted"+author:"${userId}"`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setIssues(response.data.total_count);
+        setIssueInfo(response.data.items);
+        if (userImage === null && response.data.total_count > 0) {
+          setUserImage(response.data.items[0].user.avatar_url);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const getRepo = (url) => {
+    return url.split("/")[url.split("/").length - 1];
+  };
+
+  const getDifficulty = (labels) => {
+    for (let label of labels) {
+      if (scoreMap.has(label.name)) {
+        return label.name;
+      }
+    }
+  };
+
+  const refactorData = () => {
+    const PRs = [];
+    const Issues = [];
+    let score = 0;
+
+    for (var i = 0; i < PRInfo.length; i++) {
+      const PR = PRInfo[i];
+
+      const difficulty = getDifficulty(PR.labels);
+      score += scoreMap.get(difficulty);
+
+      PRs.push({
+        link: PR.html_url,
+        title: PR.title,
+        difficulty: difficulty.split("-")[1],
+        repo: getRepo(PR.repository_url),
+      });
+    }
+
+    for (var j = 0; j < issueInfo.length; j++) {
+      const issue = issueInfo[j];
+      score += 4;
+
+      Issues.push({
+        link: issue.html_url,
+        title: issue.title,
+        repo: getRepo(issue.repository_url),
+      });
+    }
+
+    setIssueArray(Issues);
+    setPRArray(PRs);
+    setPoints(score);
+  };
 
   useEffect(() => {
     const url = window.location.pathname;
     const username = url.split("/")[2];
-    setAuthor(username);
+
+    getPRs(username);
+    getContributorIssues(username);
   }, []);
+
+  useEffect(() => {
+    if (PRInfo && issueInfo) {
+      refactorData();
+    }
+  }, [PRInfo, issueInfo]);
 
   return (
     <div
       style={{ display: "flex", justifyContent: "center", paddingTop: "50px" }}
     >
       <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+        <CardHeader
+          avatar={<Avatar aria-label="recipe" src={userImage} />}
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
