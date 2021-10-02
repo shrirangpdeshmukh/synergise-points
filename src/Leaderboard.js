731912746eb57d80248f4358f3064a8cff67f425
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2021-10-02 23:30:11 +0530

    First Version

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -23,51 +23,66 @@ const Table = () => {
   const getPRs = () => {
     axios
       .get(
-        "https://api.github.com/search/issues?q=org:Code4BBS+is:pr+is:merged"
+        'https://api.github.com/search/issues?q=org:Dummy-Organ+is:pr+is:merged+label:"syn-accepted"'
       )
       .then((response) => {
         console.log(response.data);
-        setPRInfo(response.data);
+        setPRInfo(response.data.items);
       })
       .catch((error) => console.error(error));
   };
 
   const refactorData = () => {
+    console.log("Hii");
+
     const userMap = new Map();
 
     for (var i = 0; i < PRInfo.length; i++) {
-      if (PRInfo[i].labels.length === 2) {
-        let index = PRInfo[i].labels.indexOf((e) => e.name === "syn-accepted");
+      if (PRInfo[i].labels.length > 0) {
+        let increment = 0;
+        for (let label of PRInfo[i].labels) {
+          if (scoreMap.has(label.name)) {
+            increment = scoreMap.get(label.name);
+            break;
+          }
+        }
 
-        const otherIndex = index === 0 ? 1 : 0;
+        console.log(increment);
 
-        const increment = scoreMap.get(PRInfo.labels[otherIndex].name);
-
-        if (userMap.has(PRInfo[i].user.login)) {
-          const oldData = userMap.get(PRInfo[i].user.login);
-          const newData = { ...oldData };
-          newData.score = newData.score + increment;
-          newData.pr++;
-          userMap.set(PRInfo[i].user.login, newData);
-        } else {
-          const data = {
-            image: PRInfo[i].user.avatar_url,
-            pr: 1,
-            issue: 0,
-            score: increment,
-          };
-          userMap.set(PRInfo[i].user.login, data);
+        if (increment > 0) {
+          if (userMap.has(PRInfo[i].user.login)) {
+            const oldData = userMap.get(PRInfo[i].user.login);
+            const newData = { ...oldData };
+            newData.score = newData.score + increment;
+            newData.pr++;
+            userMap.set(PRInfo[i].user.login, newData);
+          } else {
+            const data = {
+              image: PRInfo[i].user.avatar_url,
+              pr: 1,
+              issue: 0,
+              score: increment,
+            };
+            userMap.set(PRInfo[i].user.login, data);
+          }
         }
       }
-
       // console.log(userMap);
       // console.log(PRInfo);
     }
+
+    console.log([...userMap.entries()]);
   };
 
   useEffect(() => {
     getPRs();
   }, []);
+
+  useEffect(() => {
+    if (PRInfo.length > 0) {
+      refactorData();
+    }
+  }, [PRInfo]);
 
   return null;
 };
